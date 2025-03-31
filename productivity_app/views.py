987de7979a83fccc2eb1a3084198774cbde72055
yohannes2025 <johannes.m.tekle@gmail.com>
2025-03-31 16:44:19 +0200

    Add views to productivity_app

--- productivity_app/views.py
+++ productivity_app/views.py
@@ -1,3 +1,21 @@
+# productivity_app/views.py
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Task, Comment
+from .serializers import TaskSerializer, CommentSerializer
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(assigned_to=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
