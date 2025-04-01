65bcace9ab9fa81feb74299c49bb2f57cb008214
yohannes2025 <johannes.m.tekle@gmail.com>
2025-04-01 14:30:21 +0200

    Fix 'home' reference in urls.py

--- productivity_app/urls.py
+++ productivity_app/urls.py
@@ -8,5 +8,6 @@ router.register(r'tasks', views.TaskViewSet)
 router.register(r'comments', views.CommentViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', views.home, name='home'),
+    #path('', include(router.urls)),
 ]
